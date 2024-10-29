ba47d5c0636659521a09131b1f37a3cd0b3b9f1b
aaronpolo1000 <aaronpolo1000@gmail.com>
2024-10-28 21:23:10 -0300

    prueba

--- chatsocket/src/main/java/com/example/demo/service/ChatHandler.java
+++ chatsocket/src/main/java/com/example/demo/service/ChatHandler.java
@@ -1,4 +1,7 @@
 package com.example.demo.service;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.stereotype.Service;
@@ -10,6 +13,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Service
 public class ChatHandler extends TextWebSocketHandler{
 
+	private final List<String> messages = null;
 	private final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<WebSocketSession>();
 	
 	@Override
@@ -17,7 +21,9 @@ public class ChatHandler extends TextWebSocketHandler{
 		// TODO Auto-generated method stub
 		super.afterConnectionEstablished(session);
 		sessions.add(session);
-	}
+		
+		
+		}
 	
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
@@ -26,13 +32,24 @@ public class ChatHandler extends TextWebSocketHandler{
 		sessions.remove(session);
 	}
 	
+	
+	
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		// TODO Auto-generated method stub
-		super.handleTextMessage(session, message);
-		for (WebSocketSession webSocketSession : sessions) {
-			webSocketSession.sendMessage(message);
+		String receivedMessage = message.getPayload();
+		broadCastMessage(receivedMessage);
+		String keyword="websockect";
+		if(receivedMessage.toLowerCase().contains(keyword)) {
+			
+			String winnerMessage = "WebSocket es un protocolo que genera una comunicacion duplex entre cliente y servidor, en cambio que http en el cual el cliente tiene que hacer una peticion en Web Socket el servidor puede enviar datos sin que el cliente haya hecho un peticion. Ademas ambos trabajan en los mismos puertos los cuales son el puerto 80 y el puerto 443";
+	        broadCastMessage(winnerMessage);
 		}
 	}
-	
-}
+	public void broadCastMessage(String message)throws Exception {
+		for(WebSocketSession s:sessions) {
+			if(s.isOpen()) {
+				s.sendMessage(new TextMessage(message));
+				
+			}
+		}
+	}}
